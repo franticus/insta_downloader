83c72a2c0f9132129a227afda2f178ff23aaa851
S.Zelensky <zelenstein@gmail.com>
2025-03-03 23:36:21 +0300

    noreferrer

--- js/script.js
+++ js/script.js
@@ -3,9 +3,3 @@ AOS.init({
   duration: 1000, // Default duration for all animations
   once: true, // Animation triggers only once
 });
-
-// Placeholder for download functionality
-document.querySelector('.download-btn').addEventListener('click', () => {
-  window.location.href =
-    'https://uplxekx.com/fd2/?campaign=619d1b4ee5636f6c&p1=&p2=&source=adwords&site=mysite';
-});
